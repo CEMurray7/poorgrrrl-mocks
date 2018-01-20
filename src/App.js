2c121a984ff345346d0adc499d16138422b9396d
Claire Murray <claire.e.murray00@gmail.com>
2018-01-20 12:36:34 -0500

    custom font

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import './assets/fonts/Apple][.ttf';
+// import fonts from "./Apple ][.ttf";
+
+
+// import './assets/fonts/Apple][.ttf';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">POORGRRRL</h1>
+          <h1 className="App-title">P00RGRRRL</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          REL0AD
         </p>
+        <div id="root"></div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
+import WebFont from 'webfontloader';
+WebFont.load({
+  families: ['Apple ][']
+});
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
